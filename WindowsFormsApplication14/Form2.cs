83795089b1c640377a8dd2b5571fd2eee5e630a5
riasrahayu <riasrahayu7@gmail.com>
2018-02-02 21:19:17 +0700

    finishing fitur ke 2

--- WindowsFormsApplication14/Form2.cs
+++ WindowsFormsApplication14/Form2.cs
@@ -26,7 +26,44 @@ namespace WindowsFormsApplication14
 
         private void Form2_Load(object sender, EventArgs e)
         {
-           
+            try
+            {
+                string MyConnection2 = "Server=localhost;Database=visual_tugas;Uid=root;Pwd=;";
+                //Display query  
+                string Query = "SELECT * FROM `jadwal`;";
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                //  MyConn2.Open();  
+                //For offline connection we weill use  MySqlDataAdapter class.  
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = MyCommand2;
+                DataTable dTable = new DataTable();
+                MyAdapter.Fill(dTable);
+                dataGridView1.DataSource = dTable; // here i have assign dTable object to the dataGridView1 object to display data.               
+                                                   // MyConn2.Close();  
+
+                string MyConnection1 = "Server=localhost;Database=visual_tugas;Uid=root;Pwd=;";
+                string Query1 = "SELECT * FROM `pengumuman`;";
+                MySqlConnection MyConn1 = new MySqlConnection(MyConnection1);
+                MySqlCommand MyCommand1 = new MySqlCommand(Query1, MyConn1);
+                MySqlDataAdapter MyAdapter1 = new MySqlDataAdapter();
+                MyAdapter1.SelectCommand = MyCommand1;
+                DataTable dTable1 = new DataTable();
+                MyAdapter1.Fill(dTable1);
+                foreach (DataRow row in dTable1.Rows)
+                {
+                    
+                    label2.Text = (row["pengumuman"]).ToString();
+
+                }
+                
+                
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
